0cd7c51e31f6bbf320c6178c142ff77ace7d23ae
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-10 11:32:31 -0500

    attraction model completed

--- models/Attraction.js
+++ models/Attraction.js
@@ -6,7 +6,52 @@ class Attraction extends Model {}
 Attraction.init(
     {
         id: {
-            type: DataTypes.INTEGER
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        latitude: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [9]
+            }
+        },
+        longitude: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [9]
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        },
+        type_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'type',
+                key: 'id'
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING
         }
     },
     {
