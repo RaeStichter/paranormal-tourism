e8957e176b1c8074d1a69da2af8782dbbcbfecff
Giamo Lao <giamolao98@gmail.com>
2021-02-13 11:03:39 -0500

    Through relationships in index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const Category = require('./Category.js');
 const Type = require('./Type.js');
 const Comment = require('./Comment.js');
 const Vote = require('./Vote.js');
+const AttractionType = require('./AttractionType.js');
 
 User.hasMany(Attraction, { foreignKey: 'owner' });
 Attraction.belongsTo(User, { foreignKey: 'owner' });
@@ -24,6 +25,10 @@ Vote.belongsTo(User, { foreignKey: 'owner' });
 Vote.belongsTo(Attraction, { foreignKey: 'attraction_id' });
 User.hasMany(Vote, { foreignKey: 'owner' });
 Attraction.hasMany(Vote, { foreignKey: 'attraction_id' });
+
+Attraction.belongsToMany(Type, { through: AttractionType, as: 'attraction_types', foreignKey: 'attraction_id' });
+Type.belongsToMany(Attraction, { through: AttractionType, as: 'attractions_with_type', foreignKey: 'type_id' });
+
 
 // export sequelize models
 module.exports = { User, Attraction, Category, Type, Comment, Vote };
